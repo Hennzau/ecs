36c2490b81e5f884501abef2ccff0b0779fe8a37
Hennzau <dev@enzo-le-van.fr>
2023-12-11 14:38:27 +0100

    new algorithm

--- ecs/src/groups/graph.rs
+++ ecs/src/groups/graph.rs
@@ -2,9 +2,14 @@ use std::collections::{HashMap, HashSet};
 
 pub struct Graph {
     // Key = Vertex, Value = Neighbours
-    pub vertices: HashMap<i128, Vec<i128>>,
+    pub vertices: HashMap<i128, Vec<i128>>, // len p
 
-    // edge A -> B with capacity C (0 or 1)
+    // edge A -> B with capacity (C (0 or 1), and flow D)
+    pub edges: HashMap<(i128, i128), (u8, u8)>, // len q
+}
+
+pub struct Residual {
+    pub vertices: HashMap<i128, Vec<i128>>,
     pub edges: HashMap<(i128, i128), u8>,
 }
 
@@ -12,7 +17,7 @@ impl Graph {
     pub fn new() -> Self {
         Self {
             vertices: HashMap::new(),
-            edges: HashMap::new()
+            edges: HashMap::new(),
         }
     }
 
@@ -27,12 +32,12 @@ impl Graph {
 
                 if !self.edges.contains_key(&(a, b)) {
                     if c {
-                        self.edges.insert((a, b), 1);
+                        self.edges.insert((a, b), (1, 0));
                     } else {
-                        self.edges.insert((a, b), 0);
+                        self.edges.insert((a, b), (0, 0));
                     }
                 }
-            },
+            }
             None => {}
         }
     }
@@ -41,10 +46,46 @@ impl Graph {
         let edge = self.edges.get_mut(&(a, b));
         if edge.is_some() {
             if c {
-                *edge.unwrap() = 1;
+                *edge.unwrap() = (1, 0);
             } else {
-                *edge.unwrap() = 0;
+                *edge.unwrap() = (0, 0);
             }
         }
     }
+
+    pub fn construct_residual(&self) -> Residual {
+        let mut residual = Residual::new();
+
+        for ((a, b), (c, f)) in self.edges {
+            if !residual.vertices.contains_key(a) {
+                residual.vertices.insert(a, Vec::new());
+            }
+
+            if !residual.vertices.contains_key(b) {
+                residual.vertices.insert(b, Vec::new());
+            }
+
+            if c == 1 && f == 0 {
+                residual.vertices.get_mut(a).unwrap().push(b);
+                residual.edges.insert((a, b), c);
+                v
+            } else if c == 1 && f == 1 {
+                residual.vertices.get_mut(b).unwrap().push(a);
+                residual.edges.insert((b, a), c);
+            }
+        }
+
+        return residual;
+    }   // O(q)
+}
+
+impl Residual {
+    pub fn new() -> Self {
+        Self {
+            vertices: HashMap::new(),
+            edges: HashMap::new(),
+        }
+    }
+
+
 }
