7ebbc58448ae3367d9f334749c54d5fcba0963f2
Hennzau <dev@enzo-le-van.fr>
2024-02-25 21:43:48 +0100

    Graph

--- src/ecs/memory/archetype_graph.rs
+++ src/ecs/memory/archetype_graph.rs
@@ -10,12 +10,12 @@ use crate::ecs::core::component::{
 };
 
 pub struct Archetype {
-    id: ArchetypeID,
+    pub id: ArchetypeID,
 
-    components: AHashSet<ComponentID>,
+    pub components: AHashSet<ComponentID>,
 
-    next: Vec<ArchetypeID>,
-    prev: Vec<ArchetypeID>,
+    pub next: Vec<ArchetypeID>,
+    pub prev: Vec<ArchetypeID>,
 }
 
 pub struct Column {
@@ -45,5 +45,75 @@ impl ArchetypeGraph {
             columns: AHashMap::new(),
             map: AHashMap::new(),
         }
+    }
+
+    pub fn add_archetype(&mut self, archetype: Archetype) {
+        if !self.map.contains_key(&archetype.id) {
+            let size = archetype.components.len();
+            let id = archetype.id;
+
+            self.map.insert(id, size);
+
+            if !self.columns.contains_key(&size) {
+                self.columns.insert(size, Column::new());
+            }
+
+            let column = self.columns.get_mut(&size).unwrap();
+
+            let index = column.archetypes.len();
+
+            column.archetypes.push(archetype);
+            column.map.insert(id, index);
+
+            let mut current_previous = Vec::<ArchetypeID>::new();
+            let mut current_next = Vec::<ArchetypeID>::new();
+
+            let components = column.archetypes.get(index).unwrap().components.clone();
+
+            if let Some(previous) = self.columns.get_mut(&(size - 1)) {
+                for previous in &mut previous.archetypes {
+                    if previous.components.is_subset(&components) {
+                        previous.next.push(id);
+                        current_previous.push(previous.id);
+                    }
+                }
+            }
+
+            if let Some(next) = self.columns.get_mut(&(size + 1)) {
+                for next in &mut next.archetypes {
+                    if components.is_subset(&next.components) {
+                        next.prev.push(id);
+                        current_next.push(next.id);
+                    }
+                }
+            }
+
+            self.columns.get_mut(&size).unwrap().archetypes.get_mut(index).unwrap().prev = current_previous;
+            self.columns.get_mut(&size).unwrap().archetypes.get_mut(index).unwrap().next = current_next;
+        }
+    }
+
+    pub fn get(&self, id: ArchetypeID) -> Option<&Archetype> {
+        if let Some(size) = self.map.get(&id) {
+            if let Some(column) = self.columns.get(size) {
+                if let Some(index) = column.map.get(&id).cloned() {
+                    return column.archetypes.get(index);
+                }
+            }
+        }
+
+        return None;
+    }
+
+    pub fn get_mut(&mut self, id: ArchetypeID) -> Option<&mut Archetype> {
+        if let Some(size) = self.map.get(&id) {
+            if let Some(column) = self.columns.get_mut(size) {
+                if let Some(index) = column.map.get(&id).cloned() {
+                    return column.archetypes.get_mut(index);
+                }
+            }
+        }
+
+        return None;
     }
 }
